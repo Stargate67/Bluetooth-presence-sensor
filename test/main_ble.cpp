792240878d15f5b9bc0d0628f11f3bea34d662fc
Stargate67 <66971848+Stargate67@users.noreply.github.com>
2023-11-15 22:46:05 +0100

    update

--- test/main_ble.cpp
+++ test/main_ble.cpp
@@ -1,20 +1,16 @@
 #include <Arduino.h>
 #include "BLEDevice.h"
 
-struct strDevices {
-String Name;
-String Mac;
-String IP;
-};
-
-int Lampe = 33;
+int Lampara = 33;
+int Contador = 0;
 static BLEAddress *pServerAddress;
 BLEScan* pBLEScan;
 BLEClient*  pClient;
 bool deviceFound = false;
-bool Allume = false;
-
-strDevices knownDevices[5];
+bool Encendida = false;
+bool BotonOff = false;
+String knownAddresses[] = { "bc:6a:d1:b0:29:f9", "bc:6a:d1:b0:29:fc"};
+unsigned long entry;
 
 static void notifyCallback(
   BLERemoteCharacteristic* pBLERemoteCharacteristic,
@@ -32,40 +28,31 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       Serial.print("BLE Advertised Device found: ");
       Serial.println(Device.toString().c_str());
       pServerAddress = new BLEAddress(Device.getAddress()); 
+      bool known = false;
       bool Master = false;
-      int i;
-      for (i = 0; i < (sizeof(knownDevices) / sizeof(knownDevices[0])); i++) {
-        if (strcmp(pServerAddress->toString().c_str(), knownDevices[i].Mac.c_str()) == 0) {
-          Serial.print("Device found: ");
-          Serial.print(knownDevices[i].Name);
-          Serial.print(" RSSI= ");
-          Serial.println(Device.getRSSI());
-          if (Device.getRSSI() > -85) {
-            deviceFound = true;
-          }
-          else {
-            deviceFound = false;
-          }
-          Device.getScan()->stop();
-          delay(100);
+      for (int i = 0; i < (sizeof(knownAddresses) / sizeof(knownAddresses[0])); i++) {
+        if (strcmp(pServerAddress->toString().c_str(), knownAddresses[i].c_str()) == 0) 
+          known = true;
+      }
+      if (known) {
+        Serial.print("Device found: ");
+        Serial.println(Device.getRSSI());
+        if (Device.getRSSI() > -85) {
+          deviceFound = true;
         }
+        else {
+          deviceFound = false;
+        }
+        Device.getScan()->stop();
+        delay(100);
       }
     }
 };
 
 void setup() {
-
-  knownDevices[0].Name = "MI10S FY";
-  knownDevices[0].Mac = "bc:6a:d1:b0:29:fc";
-  knownDevices[0].IP = "";
-
-  knownDevices[1].Name = "XIAOMI Sous balcon";
-  knownDevices[1].Mac = "a4:c1:38:73:e7:47";
-  knownDevices[1].IP = "";
-
   Serial.begin(115200);
-  pinMode(Lampe, OUTPUT);
-  digitalWrite(Lampe, LOW);
+  pinMode(Lampara,OUTPUT);
+  digitalWrite(Lampara,LOW);
   BLEDevice::init("");
   pClient  = BLEDevice::createClient();
   pBLEScan = BLEDevice::getScan();
@@ -80,13 +67,14 @@ void Bluetooth() {
   deviceFound = false;
   BLEScanResults scanResults = pBLEScan->start(5);
   if (deviceFound) {
-    //Serial.println("Allumer la lampe");
-    Allume = true;
-    digitalWrite(Lampe, HIGH);
+    Serial.println("Encender Lamara");
+    Encendida = true;
+    digitalWrite(Lampara,HIGH);
+    Contador = 0;
     delay(10000);
   }
   else{
-    digitalWrite(Lampe, LOW);
+    digitalWrite(Lampara,LOW);
     delay(1000);
   }
 }
